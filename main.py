2ea587052e5b607324d137523a1be43a925673c6
Johnny Fox <fox.sotiras.work@gmail.com>
2024-12-21 17:24:05 +0300

    Added find functions

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ tg_bot.set_my_commands([
     telebot.types.BotCommand("/start", "Активировать бота"),
     telebot.types.BotCommand("/help",
         "Вывести список всех доступных команд"),
-    telebot.types.BotCommand("/isbn", "Найти книгу по ISBN")
+    telebot.types.BotCommand("/isbn", "Найти книгу по ISBN"),
+    telebot.types.BotCommand("/find_author", "Найти книги автора"),
+    telebot.types.BotCommand("/find_books", "Найти книги по названию")
 ])
 
 @tg_bot.message_handler(commands=["start"])
@@ -25,14 +27,21 @@ def help(message):
         (f"Список доступных команд:\n\n"
         f"/start - Активировать бота\n"
         f"/help - Вывести список всех доступных команд\n"
-        f"/isbn - Найти книгу по ISBN\n"))
+        f"/isbn <ISBN> - Найти книгу по ISBN\n"
+        f"/find_author - Найти книги автора\n"
+        f"/find_books - Найти книги по названию\n"))
 
 @tg_bot.message_handler(commands=["isbn"])
 def help(message):
     try:
         isbn = message.text.split(maxsplit=1)[1]
         data = requests\
-            .get(f"https://openlibrary.org/api/books?bibkeys=ISBN:{isbn}&format=json&jscmd=data")\
+            .get(f"https://openlibrary.org/api/books",
+            params = {
+                "bibkeys": f"ISBN:{isbn}",
+                "format": "json",
+                "jscmd": "data"
+            })\
             .json()
         book = data[f"ISBN:{isbn}"]
 
@@ -49,5 +58,67 @@ def help(message):
         tg_bot.send_message(
             message.chat.id,
             f"По этому ISBN ничего не найдено.")
+
+@tg_bot.message_handler(commands=["find_author"])
+def find_author(message):
+    tg_bot.send_message(message.chat.id, "Введите имя автора.")
+    tg_bot.register_next_step_handler(message, get_author_books)
+
+def get_author_books(message):
+    limit = 20
+    author = message.text
+    author_parse = "+".join([a for a in author.split()])
+    data = requests\
+        .get(f"https://openlibrary.org/search.json",
+        params = {
+            "author": f"{author_parse}",
+            "limit": f"{limit}"
+        })\
+        .json()
+
+    if (int(data["numFound"]) == 0):
+        tg_bot.send_message(message.chat.id,
+            "Этот автор ничего не написал :(")
+        return
+
+    books = [book["title"] for book in data["docs"]]
+    respond = ""
+    for i in range(len(books)):
+        respond += f"{i+1}. {books[i]}\n"
+
+    tg_bot.send_message(message.chat.id,
+    (f"У автора {author} есть следующие книги:\n\n"
+    f"{respond}"))
+
+@tg_bot.message_handler(commands=["find_books"])
+def find_books(message):
+    tg_bot.send_message(message.chat.id, "Введите ключевые слова.")
+    tg_bot.register_next_step_handler(message, get_books)
+
+def get_books(message):
+    limit = 20
+    title = message.text
+    title_parse = "+".join([a for a in title.split()])
+    data = requests\
+        .get(f"https://openlibrary.org/search.json",
+        params = {
+            "q": f"{title_parse}",
+            "limit": f"{limit}"
+        })\
+        .json()
+
+    if (int(data["numFound"]) == 0):
+        tg_bot.send_message(message.chat.id,
+            "Не найдено книг с таким названием :(")
+        return
+
+    books = [book["title"] for book in data["docs"]]
+    respond = ""
+    for i in range(len(books)):
+        respond += f"{i+1}. {books[i]}\n"
+
+    tg_bot.send_message(message.chat.id,
+    (f"Найдены следующие книги:\n\n"
+    f"{respond}"))
 
 tg_bot.polling(none_stop=True)
